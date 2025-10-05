c27851fe28c8847377c0f28dea80b32d38bb3ab4
Emilianodamb <emilianolrc@gmail.com>
2025-10-05 15:50:15 -0300

    Instalar librería AOS (Animate on Scroll), modificar imagen del Hero con animación.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css'
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Header, Hero, ScheduleLocation, Services, OurBrands, About, Contact, Footer} from './components';
 
 function App() {
+  // Inicializar AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // duración de la animación
+      once: true, // la animación ocurre solo una vez
+      easing: "ease-out-cubic", // tipo de animación
+      offset: 0, // dispara apenas entra al viewport
+      anchorPlacement: 'top-bottom', // cuando el top del elemento toca el bottom del viewport
+    });
+  }, []);
 
   return (
     <>
--- src/components/Hero/HeroImage.tsx
+++ src/components/Hero/HeroImage.tsx
@@ -11,14 +11,20 @@ interface HeroImageProps {
  */
 const HeroImage: React.FC<HeroImageProps> = ({ imageConfig }) => {
   return (
-    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-2/5">
+    <div 
+      className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-2/5"
+      data-aos="fade-left"
+      data-aos-delay="300"
+      data-aos-offset="-500"
+      data-aos-anchor-placement="center-center"
+    >
       <img
         src={imageConfig.src}
         alt={imageConfig.alt}
         className={imageConfig.className}
         loading="eager"
         decoding="sync"
-        // Preload hint para better performance
+        // Preload hint para mejor performance
         fetchPriority="high"
       />
     </div>
